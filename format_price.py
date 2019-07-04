4913034ad7dc13827641ccaa29c39f1fac6b2962
XATTpy <zoomerrr@mail.ru>
2019-07-04 18:58:13 +0300

    PEP 8

--- format_price.py
+++ format_price.py
@@ -6,7 +6,6 @@ def format_price(price):
         price_num = round(float(price), 2)
     except ValueError:
         return None
-
     if price_num.is_integer():
         str_price = '{:,.0f}'.format(price_num).replace(',', ' ')
     else:
@@ -20,4 +19,5 @@ if __name__ == '__main__':
     except IndexError:
         quit('At the input data is expected to format.')
         
-    print(format_price(price))
+    str_price = format_price(price)
+    print(str_price)
--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
 import unittest
-
 from format_price import format_price
 
+
 class QuadraticEquationTestCase(unittest.TestCase):
+
     def test_str_float_1(self):
         self.assertEqual(format_price('3000.0000'), '3 000')
 
